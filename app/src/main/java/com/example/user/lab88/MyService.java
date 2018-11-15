956f6054ca7736beb9f65cd5db2916a276fb91b8
m5614m5614 <m5614m5614ss@gmail.com>
2018-11-15 15:28:00 +0800

    lab8

---
+++ app/src/main/java/com/example/user/lab88/MyService.java
@@ -0,0 +1,37 @@
+package com.example.user.lab88;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+    @Override
+    public  void onCreate(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+
+                    Intent intent=new Intent(MyService.this,Main2Activity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    MyService.this.startActivity(intent);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    @Override
+    public  IBinder onBind(Intent intent){ return  null; }
+    @Override
+    public  int onStartCommand(Intent intent,int flags,int startId){
+        super.onStartCommand(intent,flags,startId);
+        return START_STICKY;
+    }
+//    public MyService() {
+//
+//
+//    }
+}
